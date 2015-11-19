e107a638402c409d4eac3a82cd353b7c812739a3
zaraki123 <puffosmo@gmail.com>
2015-11-19 22:30:59 +0100

    figa la prima volta che mi riesce una cosa giusta in un tempo umanamente accettabile: selezione sports

--- app/src/main/java/com/example/nelson/caliplay/UserProfile.java
+++ app/src/main/java/com/example/nelson/caliplay/UserProfile.java
@@ -1,21 +1,23 @@
 package com.example.nelson.caliplay;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
-import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Zaraki on 19/11/2015.
  */
+
 public class UserProfile extends AppCompatActivity implements NumberPicker.OnValueChangeListener {
 
-    private static EditText height, weight;
-    static Dialog d;
+    private static EditText height, weight, sportPast, sportPresent = null;
 
 
     @Override
@@ -24,8 +26,12 @@ public class UserProfile extends AppCompatActivity implements NumberPicker.OnVal
         setContentView(R.layout.user_profile);
         height = (EditText) findViewById(R.id.height_measurement);
         weight = (EditText) findViewById(R.id.weight_measurement);
+        sportPresent = (EditText) findViewById(R.id.sportPresent);
+        sportPast = (EditText) findViewById(R.id.sportPast);
         Button pickerHeight = (Button) findViewById(R.id.heightPicker);
         Button pickerWeight = (Button) findViewById(R.id.weightPicker);
+        Button sportPickerPresent = (Button) findViewById(R.id.sportPickerPresent);
+        Button sportPickerPast = (Button) findViewById(R.id.sportPickerPast);
         pickerHeight.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -33,6 +39,7 @@ public class UserProfile extends AppCompatActivity implements NumberPicker.OnVal
                 showHeight();
             }
         });
+
         pickerWeight.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -40,6 +47,26 @@ public class UserProfile extends AppCompatActivity implements NumberPicker.OnVal
                 showWeight();
             }
         });
+
+        sportPickerPresent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                sportPresentPicker();
+            }
+        });
+
+        sportPickerPast.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                sportPastPicker();
+            }
+        });
+    }
+
+    @Override
+    public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
 
     }
 
@@ -102,8 +129,49 @@ public class UserProfile extends AppCompatActivity implements NumberPicker.OnVal
 
     }
 
-    @Override
-    public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+
+    public void sportPresentPicker() {
+        String sportType = "sportPresent";
+        Intent sportPicker = new Intent(this, ListSports.class);
+        sportPicker.putExtra("sportType", sportType);
+        if (sportPicker.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(sportPicker, 1);
+
+        }
+
 
     }
+
+    public void sportPastPicker() {
+        String sportType = "sportPast";
+        Intent sportPicker = new Intent(this, ListSports.class);
+        sportPicker.putExtra("sportType", sportType);
+        if (sportPicker.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(sportPicker, 1);
+
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                String sportType = data.getStringExtra("sportType");
+                String selectedSport = data.getStringExtra("selectedSport");
+                if (sportType.equals("sportPresent")) {
+                    sportPresent.setText(selectedSport);
+                } else if (sportType.equals("sportPast")) {
+                    sportPast.setText(selectedSport);
+                }
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(getApplicationContext(), "Select a sport", Toast.LENGTH_SHORT).show();//Write your code if there's no result
+            }
+        }
+    }
+
+
 }
